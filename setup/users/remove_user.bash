bed59edbecfb373c672673ce114fc5e5f01d62ce
vfdev-5 <vfdev.5@gmail.com>
2021-07-17 20:59:03 +0000

    Fixed issues with remove_user.bash

--- setup/users/remove_user.bash
+++ setup/users/remove_user.bash
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+users_filepath="/shared/.userslist"
+
 # Only take action if two arguments are provided and the second is a local file
 if [ $# -eq 1 ] ; then
   USERNAME=$1
@@ -10,16 +12,15 @@ else
 fi
 
 # Need to check if user exists:
-if id "$USERNAME" &>/dev/null; then
-    echo '[INFO][$(date '+%Y-%m-%d %H:%M:%S')] User $USERNAME found'
-else
-    echo '[INFO][$(date '+%Y-%m-%d %H:%M:%S')] User $USERNAME not found'
+if ! id "$USERNAME" &>/dev/null; then
+    echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] User $USERNAME is not found"
     exit 1
 fi
 
 
-# Remove user from .userslist
-sudo sed -i '/$USERNAME `id -u $USERNAME/d' /shared/.userslist
+# Remove user from users list
+sudo sed -i "/$USERNAME `id -u $USERNAME`/d" $users_filepath
+echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] Removed $USERNAME from users list"
 # Remove existing user
 sudo deluser --remove-home $USERNAME
-echo '[INFO][$(date '+%Y-%m-%d %H:%M:%S')] User $USERNAME was deleted'
+echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] User $USERNAME was deleted"
