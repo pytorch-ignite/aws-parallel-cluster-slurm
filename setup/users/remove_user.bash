7cf6b2109a7ca0821f10f2e6b8343ac68f031edd
MrA2K2 <andrii.kovalenko.jr@gmail.com>
2021-07-18 17:46:27 +0200

    Added backup

--- setup/users/remove_user.bash
+++ setup/users/remove_user.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-
+  
 users_filepath="/shared/.userslist"
 
 # Only take action if two arguments are provided and the second is a local file
@@ -30,6 +30,17 @@ set -e
 # Remove user from users list
 sudo sed -i "/$USERNAME `id -u $USERNAME`/d" $users_filepath
 echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] Removed $USERNAME from users list"
-# Remove existing user
-sudo deluser --remove-home $USERNAME
+# Remove existing user and saving the backup
+sudo mkdir -p /home/.backup/$USERNAME
+sudo deluser --remove-home --backup-to /home/.backup/$USERNAME  $USERNAME
+# The backup is saved as username.tar.bz2 file, so in the case of the user
+# with the same username being deleted we rename the file to include the date
+# and time of deletion
+BACKUP_TIME=$(date '+%Y_%m_%d-%H_%M_%S')
+OLD_BACKUP_NAME="$USERNAME.tar.bz2"
+NEW_BACKUP_NAME="${USERNAME}_$BACKUP_TIME.tar.bz2"
+echo $NEW_BACKUP_NAME
+sudo mv /home/.backup/$USERNAME/$OLD_BACKUP_NAME /home/.backup/$USERNAME/$NEW_BACKUP_NAME
+echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] $USERNAME backup saved to /home/.backup/$NEW_BACKUP_NAME"
 echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] User $USERNAME was deleted"
+
